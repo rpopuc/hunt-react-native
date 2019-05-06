55d6edf003699dc63c2750718fb881101b2a21ab
Robson P Oliveira <rpopuc@gmail.com>
2019-05-06 02:30:41 -0300

    Adiciona página de produto, com View

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Main from './pages/main';
+import Product from './pages/product';
 
 const AppNavigator = createStackNavigator({
     Main,
+    Product,
 }, {
     defaultNavigationOptions: {
         headerStyle: {
